665b216faa0ee930c6c32e5def6b78ba06bb7b0b
Caramelo18 <fabiocaramelo18@gmail.com>
2016-05-12 23:35:53 +0100

    creating threads for cars passing their info

--- gerador.c
+++ gerador.c
@@ -8,51 +8,105 @@
 #include <sys/stat.h>
 #include <sys/file.h>
 
+#define NAMESIZE 10
 #define NUM_ENTRANCES   4
 
 unsigned int durationPeriod;
 unsigned int minInterval;
 
+struct carInfo
+{
+    char direction;
+    int number;
+    int parkingTime;
+};
 
-
-void alarm_handler(int signo){
-        printf("Time's up %d\n", signo);
-      exit(0);
+void *carThread(void *arg)
+{
+    pthread_t ownThread = pthread_self();
+    if(pthread_detach(ownThread) != 0)
+    {
+        printf("Error making thread nr %d detached\n", (int)ownThread);
+        exit(1);
+    }
+    struct carInfo *car = (struct carInfo*) arg;
+    printf("thr: car: %c%d - time: %d\n", car->direction, car->number, car->parkingTime);
+    char fifoName[6];
+    sprintf(fifoName, "fifo%c", car->direction);
+//    printf("fifoname: %s\n", fifoName);
+    return NULL;
 }
 
+void alarm_handler(int signo)
+{
+    printf("Time's up\n");
+    exit(0);
+}
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
+    if(argc != 3)
+    {
+        printf("Usage: %s <T_GERACAO> <U_RELOGIO>\n", argv[0]);
+        exit(2);
+    }
 
-  if(argc != 3) {
-          printf("Usage: gerador <T_GERACAO> <U_RELOGIO>\n");
-          exit(1);
-  }
-  durationPeriod = strtol(argv[1], NULL, 10);
-  minInterval = strtol(argv[2],NULL,10);
+    durationPeriod = strtol(argv[1], NULL, 10);
+    minInterval = strtol(argv[2],NULL,10);
 
+    if(durationPeriod <= 0 || minInterval <= 0)
+    {
+        printf("Illegal arguments. Use <unsigned int> <unsigned int>\n");
+        exit(3);
+    }
 
-  if(durationPeriod <= 0 || minInterval <= 0) {
-          printf("Illegal arguments. Use <unsigned int> <unsigned int>\n");
-          exit(2);
-  }
-  time_t t;
-  srand((unsigned) time(&t));
+    time_t t;
+    srand((unsigned) time(&t));
 
-  struct sigaction action;
-  action.sa_handler = alarm_handler;
-  sigemptyset(&action.sa_mask);
-  action.sa_flags = 0;
+    struct sigaction action;
+    action.sa_handler = alarm_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
 
-  if (sigaction(SIGALRM,&action, NULL) < 0)
-  {
-          fprintf(stderr,"Unable to install SIGALRM handler\n");
-          exit(3);
-  }
-  alarm(durationPeriod);
+    if (sigaction(SIGALRM,&action, NULL) < 0)
+    {
+        fprintf(stderr,"Unable to install SIGALRM handler\n");
+        exit(4);
+    }
+    alarm(durationPeriod);
 
-  char *fifoNames[NUM_ENTRANCES]= {"N", "S", "E", "W"};
-  int index = rand() % 4;
-  char *direction = fifoNames[index];
+    time_t begin, end;
+    char *fifoNames[NUM_ENTRANCES] = {"N", "S", "E", "W"};
+    int probabilities[10] = {0,0,0,0,0,1,1,1,2,2};
+//    int carNumbers[4] = {0, 0, 0, 0};
+    int carNumber = 0;
+    int index;
+    int sleepTime = probabilities[rand() % 10] * minInterval;
+
+    begin = clock();
+    while(1)
+    {
+        end = clock();
+        if(end - begin >= sleepTime)
+        {
+            index = rand() % 4;
+            char *direction = fifoNames[index];
+            //++carNumbers[index];
+            ++carNumber;
+            int parkTime = ((rand() % 10) + 1) * minInterval;
+            sleepTime = probabilities[rand() % 10] * minInterval;
+        //    printf("car: %s%d - time: %d\n", direction, carNumber, parkTime);
+            begin = end;
+
+            pthread_t newThread;
+            struct carInfo car;
+            car.direction = *direction;
+            car.number = carNumber;
+            car.parkingTime = parkTime;
+
+            pthread_create(&newThread, NULL, carThread, (void *)&car);
+        }
+    }
 
 
 }
--- parque.c
+++ parque.c
@@ -14,62 +14,64 @@ unsigned int maxSpaces;
 
 #define NUM_ENTRANCES   4
 
-void alarm_handler(int signo){
-        printf("Time's up %d\n", signo);
-      exit(0);
+void alarm_handler(int signo)
+{
+    printf("Time's up\n");
+    exit(0);
 }
 
-void *entrancePoints(void *arg){
-        char fifoName[6]; //tipo nao era preciso fazer tao grande porque sabes que vai ser so "fifoN"
-        sprintf(fifoName, "fifo%s", (char *)arg);
-        printf("Controller: %s\n", fifoName);
-        int fd;
-        mkfifo(fifoName, 0660);
-        fd = open(fifoName, O_RDONLY);
-        close(fd);
-        unlink(fifoName);
-        return NULL;
+void *entrancePoints(void *arg)
+{
+    char fifoName[6]; //tipo nao era preciso fazer tao grande porque sabes que vai ser so "fifoN"
+    sprintf(fifoName, "fifo%s", (char *)arg);
+    printf("Controller: %s\n", fifoName);
+    int fd;
+    mkfifo(fifoName, 0660);
+    fd = open(fifoName, O_RDONLY);
+    close(fd);
+    unlink(fifoName);
+    return NULL;
 }
 
 int main(int argc, char* argv[]){
 
-        if(argc != 3) {
-                printf("Usage: parque <N_LUGARES> <T_ABERTURA>\n");
-                exit(1);
-        }
-        unsigned int workingTime;
-        workingTime = strtol(argv[2],NULL,10);
-        maxSpaces = strtol(argv[1], NULL, 10);
-        parkingSpaces = 0;
+    if(argc != 3) {
+        printf("Usage: parque <N_LUGARES> <T_ABERTURA>\n");
+        exit(1);
+    }
+    unsigned int workingTime;
+    workingTime = strtol(argv[2],NULL,10);
+    maxSpaces = strtol(argv[1], NULL, 10);
+    parkingSpaces = 0;
 
-        if(workingTime <= 0 || maxSpaces <= 0) {
-                printf("Illegal arguments. Use positive <unsigned int> <unsigned int>\n");
-                exit(2);
-        }
+    if(workingTime <= 0 || maxSpaces <= 0) {
+        printf("Illegal arguments. Use positive <unsigned int> <unsigned int>\n");
+        exit(2);
+    }
 
-        struct sigaction action;
-        action.sa_handler = alarm_handler;
-        sigemptyset(&action.sa_mask);
-        action.sa_flags = 0;
+    struct sigaction action;
+    action.sa_handler = alarm_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
 
-        if (sigaction(SIGALRM,&action, NULL) < 0)
-        {
-                fprintf(stderr,"Unable to install SIGALRM handler\n");
-                exit(3);
-        }
-        alarm(workingTime);
-        //alarm(1);
+    if (sigaction(SIGALRM,&action, NULL) < 0)
+    {
+        fprintf(stderr,"Unable to install SIGALRM handler\n");
+        exit(3);
+    }
+    alarm(workingTime);
+    //alarm(1);
 
-        //while(1) ;
+    //while(1) ;
 
 
-        pthread_t threads[NUM_ENTRANCES];
-        char *fifoNames[NUM_ENTRANCES]= {"N", "S", "E", "W"};
-        int i;
-        for (i = 0; i < NUM_ENTRANCES; i++) {
-                pthread_create(&threads[i], NULL, entrancePoints, fifoNames[i]);
+    pthread_t threads[NUM_ENTRANCES];
+    char *fifoNames[NUM_ENTRANCES]= {"N", "S", "E", "W"};
+    int i;
+    for (i = 0; i < NUM_ENTRANCES; i++) {
+        pthread_create(&threads[i], NULL, entrancePoints, fifoNames[i]);
 
-        }
-        pthread_exit(0);
-        return 0;
+    }
+    pthread_exit(0);
+    return 0;
 }
